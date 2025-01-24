a84fffa8f3f99bc2e03f6480570a9028982cf7fb
Ede Sufiatti <sufiattie@gmail.com>
2025-01-24 19:54:46 -0300

    c2

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Express } from "express";
+
+class App {
+
+  private readonly port: number = 5000;
+  private _app: Express;
+
+
+
+  constructor() {
+    this._app = express();
+  }
+
+
+  public configure() {
+
+  }
+
+  public strat() {
+    this._app.listen(this.port, (error) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(`Servidor rodando na porta ${this.port}`);
+    })
+
+
+  }
+
+
+
+
+
+
+}
+
+export const app = new App();
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import ContaCorrente from "./classes/conta-corrente";
+import { app } from "./app";
 
 
-
-const conta = new ContaCorrente("123-4", 1001, "123.456.789-00", "Fulano", new Date(), new Date());
-console.log(conta);
-
-console.log("Saldo inicial  " + conta.saldo);
-
-
-conta.setSaldo(25000, "C");
-conta.setSaldo(3000, "D");
-console.log("Saldo atual  " + conta.saldo);
-
-try {
-  conta.setSaldo(-1, "D");
-} catch (error: any) {
-  console.log(error.message);
-}
-
-
-// criar função para extrato
-
+(async () => {
+  app.configure();
+  app.strat();
+})()
